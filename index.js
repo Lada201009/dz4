688fbd25f0a5c78b6f2cbdc64d3ac047d72827ec
Lada <lada.goncharova@gmail.com>
2022-06-11 01:15:41 +0300

    Исправила ошибки

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let n = +prompt("Введите число от 1 до 5", 5);
+let n = +prompt("Введите номер задания от 1 до 5", 5);
 function Task1() {
   let a = 5,
     b = true,
@@ -7,39 +7,44 @@ function Task1() {
   console.log(a, b, c, d);
 }
 function Task2() {
-  let greeting = "Hello, ",
-    x;
+  let greeting = "Hello, ";
   let userName = prompt(`user name `);
-  x = userName;
-  console.log(greeting, x);
+  console.log(greeting, userName);
+  alert(`Hallo, ${userName}`);
 }
 function Task3() {
-  let min = prompt(`number `);
+  let min = prompt(`enter minutes, 60`);
   if (min >= 0 && min <= 14) {
     alert("В первую четверть.");
   }
-  if (min >= 15 && min <= 30) {
+  else if (min >= 15 && min <= 30) {
     alert("Во вторую четверть.");
   }
-  if (min >= 31 && min <= 45) {
+  else if (min >= 31 && min <= 45) {
     alert("В третью четверть.");
   }
-  if (min >= 46 && min <= 59) {
+  else {
     alert("В четвертую четверть.");
   }
 }
 function Task4() {
   let num = 5;
   for (; num <= 50; num++) {
-    if (num % 5 === 0) console.log(num);
+    if (num % 5 === 0) 
+    console.log(num);
   }
 }
 function Task5() {
-  let mult = 1;
-  for (let i = 5; i <= 12; i++) {
+  let limit1 = prompt("Введите минимальное число ");
+   let limit2 = prompt("Введите максимальное число ");
+   let mult = 1;
+   for (let i = limit1; i<=limit2; i++){
     mult = mult * i;
-  }
-  console.log(mult);
+   }
+   console.log(limit1, limit2);
+   console.log(mult);
+   alert(mult);
+   return mult;
 }
 
 switch (n) {
